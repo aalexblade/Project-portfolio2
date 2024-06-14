73fdeeebd33145da4b5c685a688ca29f594ec974
aalexblade <79399963+aalexblade@users.noreply.github.com>
2024-06-14 21:04:04 +0300

    mob menu is open

--- js/modal.js
+++ js/modal.js
@@ -14,20 +14,3 @@
 })();
 
 
-// mobail open-close button menu
-
-(() => {
-    const menuBtnRef = document.querySelector("[data-menu-buttun]");
-    // const mobileMenuRef = document.querySelector("[data-menu]");
-
-    menuBtnRef.addEventListener("click", () => {
-        // const expanded =
-        //     menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-        menuBtnRef.classList.toggle("is-open");
-        // menuBtnRef.setAttribute("artia-expanded", !expanded);
-
-        // mobileMenuRef.classList.toggle("is-open")
-    });
-});
-
