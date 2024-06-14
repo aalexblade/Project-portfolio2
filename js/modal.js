cc5c3439e711d11509c007e2c4ce7c3f8c21da5d
aalexblade <79399963+aalexblade@users.noreply.github.com>
2024-06-14 17:13:15 +0300

    mob-menu-icon

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,22 @@
         refs.modal.classList.toggle("is-hidden");
     }
 })();
+
+
+// mobail open-close button menu
+
+(() => {
+    const menuBtnRef = document.querySelector("[data-menu-buttun]");
+    // const mobileMenuRef = document.querySelector("[data-menu]");
+
+    menuBtnRef.addEventListener("click", () => {
+        // const expanded =
+        //     menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+        menuBtnRef.classList.toggle("is-open");
+        // menuBtnRef.setAttribute("artia-expanded", !expanded);
+
+        // mobileMenuRef.classList.toggle("is-open")
+    });
+});
+
